bf9cc924d43a73fbc5176448a70de8021d9d8e0b
jaanonim <mat8mro@gmail.com>
2025-10-22 15:54:12 +0200

    add github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+
+export default defineConfig(({ command, mode }) => {
+    if (command === "build") {
+        const env = loadEnv(mode, process.cwd(), "");
+        return {
+            base: env.BASE_URL,
+            plugins: [react()],
+        };
+    }
+    return { plugins: [react()] };
+});
