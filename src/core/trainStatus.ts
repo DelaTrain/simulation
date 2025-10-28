fa12b7dd54bcc782a780eda03586d186c0eac37a
jaanonim <mat8mro@gmail.com>
2025-10-28 16:21:31 +0100

    using normal enum

--- src/core/trainStatus.ts
+++ src/core/trainStatus.ts
@@ -1,11 +1,4 @@
-import { makeBrandedType } from "./makeBrandedType";
-
-/**
- * Quasi-enum saying if the Train is waiting or going
- */
-export const TrainStatus = makeBrandedType({
-    Waiting : "0",
-    NotWaiting : "1"
-}, 'trainStatus') ;
-
-export type TrainStatus = (typeof TrainStatus)[keyof typeof TrainStatus];
+export enum TrainStatus {
+    Waiting = "0",
+    NotWaiting = "1",
+}
